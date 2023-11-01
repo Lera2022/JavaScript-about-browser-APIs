bb10db36d191a41aa52689a064e1a6dec66f2e8d
Lera <valeriiakolesnikova21@gmail.com>
2023-11-01 19:38:46 +0500

    homework 2 finished

--- Lesson 2. Events, forms/homework/scripts/script.js
+++ Lesson 2. Events, forms/homework/scripts/script.js
@@ -1,6 +1,9 @@
 const slider = document.querySelector('.slider');
 const prevButton = document.querySelector('.prev-button');
 const nextButton = document.querySelector('.next-button');
+const firstButton = document.querySelector('.first-button')
+const secondButton = document.querySelector('.second-button')
+const thirdButton = document.querySelector('.third-button')
 const slides = Array.from(slider.querySelectorAll('img'));
 const slideCount = slides.length;
 let slideIndex = 0;
@@ -12,6 +15,21 @@ prevButton.addEventListener('click', () => {
 
 nextButton.addEventListener('click', () => {
   slideIndex = (slideIndex + 1) % slideCount;
+  slide();
+});
+
+firstButton.addEventListener('click', () => {
+  slideIndex = 0;
+  slide();
+});
+
+secondButton.addEventListener('click', () => {
+  slideIndex = 1;
+  slide();
+});
+
+thirdButton.addEventListener('click', () => {
+  slideIndex = 2;
   slide();
 });
 
